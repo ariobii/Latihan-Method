47b5929e20456f2210c50b8b93525a3f4902cee7
ariobii <auliabimo5@gmail.com>
2019-04-02 14:38:07 +0700

    latihan method pertemuan 5

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letihanMethod
+{
+    public class Calculator
+    {
+        public int Penjumlah(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Penjumlah(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Pengurang(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hasil;
+
+            Calculator calc = new Calculator();
+
+            hasil = calc.Penjumlah(10, 2);
+            CetakHasil("Hasil Penjumlahan : " + hasil);
+
+            hasil = calc.Penjumlah(10, 2, 3);
+            CetakHasil("Hasil Penjumlahan : " + hasil);
+
+            hasil = calc.Pengurang(7, 2);
+            CetakHasil("Hasil Pengurangan : " + hasil);
+
+            hasil = Calculator.Perkalian(5, 2);
+            CetakHasil("Hasil Perkalian : " + hasil);
+
+            Console.ReadKey();
+        }
+
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+
+    
+}
